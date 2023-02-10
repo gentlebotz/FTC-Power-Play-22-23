0701133298a934214693440e1d7e00c0e481dd59
Coaster_Playz <52416875+CoasterPlayz@users.noreply.github.com>
2023-02-10 23:11:05 +0100

    Update parking auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForwardAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForwardAuto.java
@@ -27,6 +27,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.apriltag.AprilTagDetection;
+import java.util.ArrayList;
 
 import static android.os.SystemClock.currentThreadTimeMillis;
 import static android.os.SystemClock.setCurrentTimeMillis;
@@ -88,32 +90,67 @@ public class ForwardAuto extends LinearOpMode {
 
     LiftState liftState = LiftState.LIFT_START;
 
-    private int lowPole = 0;
-    private int midPole = 0;
-    private int highPole = 0;
+    private int lowPole = 1630;
+    private int midPole = 3000;
+    private int highPole = 4500;
+    private int stackHight = 1000;
 
-    private double intakeArmPickupPosition = 0.2;
+    private double intakeArmPickupPosition = 0.8;
     private double intakeArmMidPosition = 0.5;
-    private double intakeArmDropPosition = 0.8;
+    private double intakeArmFrontDropPosition = 0.2;
+    private double intakeArmDropPosition = 0;
+
+    private boolean initHandClosed = false;
+    private boolean initHandPressed = false;
+    private double handOpenPos = 0.29;
+    private double handClosedPos = 0.1;
 
     private enum ArmState {
         ARM_INTAKE,
-        ARM_MID,
+        ARM_MID_TO_DROP,
+        ARM_MID_TO_INTAKE,
         ARM_DROP
     }
 
     private boolean aPressed = false;
 
-    private OpenCvCamera webcam;
-
-    private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
-    private static final int CAMERA_HEIGHT = 480; // height of wanted camera resolution
-
-    ArmState armState = ArmState.ARM_INTAKE;
+    ArmState armState = ArmState.ARM_MID_TO_DROP;
 
     public static double mapRange(double a1, double a2, double b1, double b2, double s) {
         return b1 + ((s - a1) * (b2 - b1)) / (a2 - a1);
     }
+
+    private boolean StartLeft = true;
+
+    /*
+    OpenCV Vars
+    */
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    int numFramesWithoutDetection = 0;
+
+    final float DECIMATION_HIGH = 3;
+    final float DECIMATION_LOW = 2;
+    final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
+    final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
+
+    private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 480; // height of wanted camera resolution
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -204,38 +241,77 @@ public class ForwardAuto extends LinearOpMode {
         Initialize EasyOpenCV
          */
 
-        // OpenCV webcam
+        int detectedID = 0;
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
-        // OpenCV Pipeline
-        powerplayPipeline myPipeline;
-        webcam.setPipeline(myPipeline = new powerplayPipeline()); // Was PipeLine
-
-        // Configuration of Pipeline
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
             {
-                webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(CAMERA_WIDTH,CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
             public void onError(int errorCode)
             {
-                // Ignore error and hope it works
+
             }
         });
 
         telemetry.addData("Status: ", "Done");
         telemetry.update();
 
-        waitForStart();
+        while(!isStarted() && !isStopRequested()){
+            // Calling getDetectionsUpdate() will only return an object if there was a new frame
+            // processed since the last time we called it. Otherwise, it will return null. This
+            // enables us to only run logic when there has been a new frame, as opposed to the
+            // getLatestDetections() method which will always return an object.
+            ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
 
-        if (myPipeline.error) {
-            telemetry.addData("Exception: ", myPipeline.debug);
+            // If there's been a new frame
+            if(detections != null)
+            {
+                // We don't see any tags
+                if(detections.size() == 0)
+                {
+                    numFramesWithoutDetection++;
+
+                    // If we haven't seen a tag for a few frames, lower the decimation
+                    // so we can hopefully pick one up if we're e.g. far back
+                    if(numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_LOW);
+                    }
+                }
+                // We do see tags!
+                else
+                {
+                    numFramesWithoutDetection = 0;
+
+                    // If the target is within 1 meter, turn on high decimation to
+                    // increase the frame rate
+                    if(detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
+                    }
+
+                    for(AprilTagDetection detection : detections)
+                    {
+                        detectedID = detection.id;
+                    }
+                }
+            }
+
+            telemetry.addLine(String.format("\nDetected tag ID=%d", detectedID));
+            telemetry.update();
         }
+
+        waitForStart();
 
         switch(myPipeline.getLocation()) {
             case MID:
